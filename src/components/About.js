1e1610b7c19022d8f5097e5db858f4082292d6c7
Tao Bian <biantao0519@hotmail.com>
2022-11-08 10:24:18 +1100

    update some detail for porjects and resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import Photo from '../images/photo.png'
+import Photo from '../images/profile_photo.jpg'
 import '../style/About.css'
 
 const imgStyle = {
-  padding: '20px'
+  padding: '20px',
+  height: '300px',
+  width: '250px'
+}
+
+const statementStyle ={
+  paddingLet: '50px',
+  width: '60%',
+  margin: '0 auto'
 }
 
 function About() {
@@ -13,8 +21,15 @@ function About() {
         <h1 className="title">About Me</h1>
         <hr />
         <img className="photo" style={imgStyle} src={Photo} alt="Fred Bian" />
-        <p className="content">
-          Hi Everyone, I'm Fred, currently learning coding in Monash University Coding Bootcamp.
+        <p style={statementStyle} className="content">
+          Hi Everyone, I'm Fred, a Front-end | Full-Stack Web Developer leveraging a background in accounting to build a more
+          intuitive user experience on websites and software platforms. Recently earned a certificate in
+          full stack development from Monash University Coding Boot Camp, with newly developed
+          skills in JavaScript, CSS, React.js, Node.js, SQL, NoSQL and responsive web design.
+          Strengths in creativity, teamwork, and building projects from ideation to execution. Innovative
+          problem-solver who is passionate about developing apps with a focus on mobile-first design
+          and development.
+
         </p>
         <p className="content">
           Apart from working and coding, I like to reading, travelling and fishing.
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,9 +2,10 @@ import React from 'react'
 import "../style/Portfolio.css"
 import Projects from './Projects'
 import { Container, Row, Col } from 'react-bootstrap'
-import codequize from '../images/codequize.png'
+import cocktail_recipe_finder from '../images/cocktail_recipe_finder.png'
 import password_generator from '../images/password_generator.png'
-import protfolio1 from '../images/protfolio1.png'
+import paws_for_homes from '../images/paws_for_homes.png'
+import easy_rent from '../images/easy_rent.png'
 import tech_blog from '../images/tech_blog.png'
 import weather_dashboard from '../images/weather_dashboard.png'
 import work_day_scheduler from '../images/work_day_scheduler.png'
@@ -21,29 +22,47 @@ function Protfolio() {
                     <Row className="row" style={{ justifyContent: "center", paddingBottom: "10px" }}>
                         <Col md={4} className="project-card">
                             <Projects
-                                imgPath={protfolio1}
-                                title="Protfolio1"
-                                description="A protfolio of work, built with HTML and CSS."
-                                linkApp="https://fredbian.github.io/MONU-FSF-Module2_Challenge/"
-                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module2_Challenge"
+                                imgPath={easy_rent}
+                                title="Easy Rent"
+                                description="Interactive MERN Stack Single-Page Application"
+                                linkApp="https://easy-rent-2022.herokuapp.com/"
+                                linkGithub="https://github.com/Fredbian/easy-rent"
                             />
                         </Col>
                         <Col md={4} className="projects">
                             <Projects
-                                imgPath={codequize}
-                                title="Codequize"
-                                description="A JavaScript Code Quiz challenge."
-                                linkApp="https://fredbian.github.io/MONU-FSF-Module4_Challenge/"
-                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module4_Challenge"
+                                imgPath={tech_blog}
+                                title="Tech Blog"
+                                description="A CMS-style blog site, following the MVC paradigm"
+                                linkApp="https://module-14-tech-blog-fred.herokuapp.com/"
+                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module-14-Challenge-Tech-Blog"
                             />
                         </Col>
                         <Col md={4} className="projects">
                             <Projects
-                                imgPath={password_generator}
-                                title="Password Generator"
-                                description="A Javascript password generator."
-                                linkApp="https://fredbian.github.io/MONU-FSF-Module3_Challenge-/"
-                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module3_Challenge-"
+                                imgPath={weather_dashboard}
+                                title="Weather Dashboard"
+                                description="Weather app that provides current and future weather forecasts in cities"
+                                linkApp="https://fredbian.github.io/MONU-FSF-Module-6-Challenge-Weather-Dashboard/"
+                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module-6-Challenge-Weather-Dashboard"
+                            />
+                        </Col>
+                        <Col md={4} className="projects">
+                            <Projects
+                                imgPath={paws_for_homes}
+                                title="Paws For Homes"
+                                description="Interactive Node-based Full-Stack application following the MVC"
+                                linkApp="https://paws-for-homes.herokuapp.com/"
+                                linkGithub="https://github.com/Zoki91/Paws-For-Homes"
+                            />
+                        </Col>
+                        <Col md={4} className="projects">
+                            <Projects
+                                imgPath={cocktail_recipe_finder}
+                                title="Cocktail Recipe Finder"
+                                description="A mobile first interactive Front-end application"
+                                linkApp="https://dark40.github.io/cocktail-recipe-party-activity/"
+                                linkGithub="https://github.com/dark40/cocktail-recipe-party-activity"
                             />
                         </Col>
                         <Col md={4} className="projects">
@@ -53,24 +72,6 @@ function Protfolio() {
                                 description="It's a simple Work Day Scheduler Calendar application build with HTML, CSS and JS."                            
                                 linkApp="https://fredbian.github.io/MONU-FSF-Module5_Challenge-Work-Day-Scheduler/"
                                 linkGithub="https://github.com/Fredbian/MONU-FSF-Module5_Challenge-Work-Day-Scheduler"
-                            />
-                        </Col>
-                        <Col md={4} className="projects">
-                            <Projects
-                                imgPath={weather_dashboard}
-                                title="Weather Dashboard"
-                                description="Use OpenWeather API to fetch weather informaton."
-                                linkApp="https://fredbian.github.io/MONU-FSF-Module-6-Challenge-Weather-Dashboard/"
-                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module-6-Challenge-Weather-Dashboard"
-                            />
-                        </Col>
-                        <Col md={4} className="projects">
-                            <Projects
-                                imgPath={tech_blog}
-                                title="Tech Blog"
-                                description="A CMS-style blog site, following the MVC paradigm."
-                                linkApp="https://module-14-tech-blog-fred.herokuapp.com/"
-                                linkGithub="https://github.com/Fredbian/MONU-FSF-Module-14-Challenge-Tech-Blog"
                             />
                         </Col>
                     </Row>
