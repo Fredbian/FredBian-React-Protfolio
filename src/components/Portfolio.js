6a4b17669162303e10bf69b21086b57638f2dfec
Tao Bian <biantao0519@hotmail.com>
2022-11-22 08:40:46 +1100

    update tec

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -23,7 +23,7 @@ function Protfolio() {
                             <Projects
                                 imgPath={easy_rent}
                                 title="Easy Rent"
-                                description="Interactive MERN Stack Single-Page Application"
+                                description="Interactive MERN Stack Single-Page Application. Tec: React, Node, JavaScript, MERN stack, MongoDB, JWT, GraphQL, Nodemailer, Stripe"
                                 linkApp="https://easy-rent-2022.herokuapp.com/"
                                 linkGithub="https://github.com/Fredbian/easy-rent"
                             />
@@ -32,7 +32,7 @@ function Protfolio() {
                             <Projects
                                 imgPath={tech_blog}
                                 title="Tech Blog"
-                                description="A CMS-style blog site, following the MVC paradigm"
+                                description="A CMS-style blog site, following the MVC paradigm. Tec: JavaScript, MySQL, Node, ORM, Bcrypt"
                                 linkApp="https://module-14-tech-blog-fred.herokuapp.com/"
                                 linkGithub="https://github.com/Fredbian/MONU-FSF-Module-14-Challenge-Tech-Blog"
                             />
@@ -41,7 +41,7 @@ function Protfolio() {
                             <Projects
                                 imgPath={weather_dashboard}
                                 title="Weather Dashboard"
-                                description="Weather app that provides current and future weather forecasts in cities"
+                                description="Weather app that provides current and future weather forecasts in cities. Tec: HTML, CSS, JavaScript, Openweather API"
                                 linkApp="https://fredbian.github.io/MONU-FSF-Module-6-Challenge-Weather-Dashboard/"
                                 linkGithub="https://github.com/Fredbian/MONU-FSF-Module-6-Challenge-Weather-Dashboard"
                             />
@@ -50,7 +50,7 @@ function Protfolio() {
                             <Projects
                                 imgPath={paws_for_homes}
                                 title="Paws For Homes"
-                                description="Interactive Node-based Full-Stack application following the MVC"
+                                description="Interactive Node-based Full-Stack application following the MVC. Tec: Express, Node, Bootstrap, JavaScript, MySQL, ORM, Bcrypt"
                                 linkApp="https://paws-for-homes.herokuapp.com/"
                                 linkGithub="https://github.com/Zoki91/Paws-For-Homes"
                             />
@@ -59,7 +59,7 @@ function Protfolio() {
                             <Projects
                                 imgPath={cocktail_recipe_finder}
                                 title="Cocktail Recipe Finder"
-                                description="A mobile first interactive Front-end application"
+                                description="A mobile first interactive Front-end application. Tec: HTML, CSS, JavaScript, APIs"
                                 linkApp="https://dark40.github.io/cocktail-recipe-party-activity/"
                                 linkGithub="https://github.com/dark40/cocktail-recipe-party-activity"
                             />
